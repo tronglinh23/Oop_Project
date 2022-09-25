995c42fe2bdceff2a6ca62c1aa21c1ee0f08b0e7
21020382-Quan <qbuiminh5@gmail.com>
2022-09-25 13:25:54 +0700

    add the enemy

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Enemy {
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+
+public class Enemy {
+    private int x;
+    private int y;
+    private int speed = 3;
+    private AnchorPane enemyPane;
+    private ImageView enemy;
+    private Scene enemyScene;
+    private Stage enemyStage;
+    private int imageIndex;
+
+    public void Enemy(int x, int y, AnchorPane enemyPane, Scene enemyScene) {
+        this.x = x;
+        this.y = y;
+        this.enemyPane = enemyPane;
+        this.enemyScene = enemyScene;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+
+
 }

e5cc39ffbec8738ee513a281cd30d351272b667d
21020382-Quan <qbuiminh5@gmail.com>
2022-09-25 21:11:35 +0700

    enemy move

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -4,20 +4,28 @@ import javafx.stage.Stage;
 
 import javax.swing.text.html.ImageView;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Enemy {
     private int x;
     private int y;
     private int speed = 3;
+    private int orient;
     private AnchorPane enemyPane;
     private ImageView enemy;
     private Scene enemyScene;
     private Stage enemyStage;
     private int imageIndex;
 
-    public void Enemy(int x, int y, AnchorPane enemyPane, Scene enemyScene) {
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int UP = 2;
+    public static final int DOWN = 3;
+
+    public void Enemy(int x, int y, int orient, AnchorPane enemyPane, Scene enemyScene) {
         this.x = x;
         this.y = y;
+        this.orient = orient;
         this.enemyPane = enemyPane;
         this.enemyScene = enemyScene;
     }
@@ -38,6 +46,29 @@ public class Enemy {
         return this.y;
     }
 
+    public void setOrient(int orient){
+        this.orient = orient;
+    }
+    public int getOrient() {
+        return this.orient;
+    }
 
-
+    public void moveEnemy(ArrayList<TileMap> arrTileMap, ArrayList<Boom> arrBoom, int t) {
+        int speed = 2;
+        int xRaw = x;
+        int yRaw = y;
+        switch (orient) {
+            case LEFT:
+                xRaw -= speed;
+                break;
+            case RIGHT:
+                xRaw += speed;
+                break;
+            case UP:
+                yRaw -= speed;
+                break;
+            case DOWN:
+                yRaw += speed;
+            default:
+        }
 }
